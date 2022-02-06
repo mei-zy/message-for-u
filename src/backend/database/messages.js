8ab7fbef42007b3a2b31efa423c0bcc41077ad7f
urther <alswlkku@ajou.ac.kr>
2022-02-06 20:54:09 +0900

    🌱 chore : init backend setting

---
+++ src/backend/database/messages.js
@@ -0,0 +1,7 @@
+export const messages = [
+  {
+    writer: '김철수',
+    content: '새해 복 많이 받으라굿',
+    createdAt: '20201-12-10',
+  },
+];
---
+++ src/js/helper.js
@@ -0,0 +1,1 @@
+export const $ = querySelector => document.querySelector(querySelector);
